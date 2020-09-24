31d34029e6e00a77d9bbcb36c2b2e239e786d64e
actuallyDan <dankral01@gmail.com>
2020-09-24 05:13:45 -0400

    added web customization and fixed env checker

--- env.js
+++ env.js
@@ -3,7 +3,6 @@
  * path: '/environment.js' (root of your project)
  ******************************/
 
-import Constants from "expo-constants";
 import { ENV } from "./vars.js";
 
 // const localhost = Platform.OS === 'ios' ? 'localhost:8080' : '10.0.2.2:8080';
@@ -16,16 +15,14 @@ const SECURE_CONSTANTS = {
     "https://athares-images.s3.us-east-2.amazonaws.com/Athares-logo-small-white.png",
 };
 
-const getEnvVars = (env = Constants.manifest.releaseChannel) => {
+const getEnvVars = () => {
   // What is __DEV__ ?
   // This variable is set to true when react-native is running in Dev mode.
   // __DEV__ is true when run locally, but false when published.
   if (__DEV__) {
     return { ...SECURE_CONSTANTS, ...ENV.dev };
-  } else if (env === "staging") {
-    return { ...SECURE_CONSTANTS, ...ENV.staging };
-  } else if (env === "prod") {
-    return { ...ENV.prod, ...SECURE_CONSTANTS };
+  } else {
+    return { ...SECURE_CONSTANTS, ...ENV.prod };
   }
 };
 
