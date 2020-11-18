faa63dcba846f3fd3cdca942480aba9e7fff5692
actuallyDan <dankral01@gmail.com>
2020-11-18 16:21:21 -0500

    potentially fix unhandled exception on login by upgrading

--- utils/useImperativeQuery.js
+++ utils/useImperativeQuery.js
@@ -1,5 +1,6 @@
 import { useQuery } from "@apollo/client";
 
+// hook to make queries like mutation calls so we don't have to re-render
 export default function useImperativeQuery(query) {
   const { refetch } = useQuery(query, { skip: true });
 
